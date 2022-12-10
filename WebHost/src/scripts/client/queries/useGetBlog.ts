6398e0e1f3f598f3c7da4f6a10b871e76aca94c2
harrygillingham99 <harrygillingham99@gmail.com>
2022-12-10 00:32:43 +0000

    fix: method name change

--- WebHost/src/scripts/client/queries/useGetBlog.ts
+++ WebHost/src/scripts/client/queries/useGetBlog.ts
@@ -3,7 +3,7 @@ import AppState from "state/AppState";
 
 const useGetBlog = (slug: string) => {
   const { client } = AppState.useContainer();
-  const query = useQuery(["blog", slug], () => client.blog_Article(slug));
+  const query = useQuery(["blog", slug], () => client.blog_ArticleGET(slug));
   return { ...query };
 };
 
