082793691c31a60cfe392423503a0c4bb874f4cb
harrygillingham99 <harrygillingham99@gmail.com>
2023-02-17 18:20:50 +0000

    fix: tidy up, remove duplicated code

--- WebHost/Controllers/AdminController.cs
+++ WebHost/Controllers/AdminController.cs
@@ -2,26 +2,16 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using NSwag.Annotations;
-using harrygillingham.xyz.Objects.Config;
-using Microsoft.Extensions.Options;
 
 namespace harrygillingham.xyz.WebHost.Controllers
 {
     [OpenApiIgnore]
     public class AdminController : Controller
     {
-        private readonly BlogConfig _blogConfig;
-        public AdminController(IOptions<BlogConfig> blogConfig)
-        {
-            _blogConfig = blogConfig.Value;
-        }
-
         [Authorize]
         public IActionResult Index()
         {
-            return View("Index", _blogConfig
-                .SetIsAuthenticated(User.Identity)
-                .SetLogOutUrl(Url.Action("Logout", "Account")!));
+            return View("Index");
         }
     }
 }
--- WebHost/Controllers/HomeController.cs
+++ WebHost/Controllers/HomeController.cs
@@ -1,7 +1,5 @@
-﻿using harrygillingham.xyz.Objects.Config;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Options;
 using NSwag.Annotations;
 
 namespace harrygillingham.xyz.WebHost.Controllers
@@ -9,18 +7,11 @@ namespace harrygillingham.xyz.WebHost.Controllers
     [OpenApiIgnore]
     public class HomeController : Controller
     {
-        private readonly BlogConfig _blogConfig;
-        public HomeController(IOptions<BlogConfig> blogConfig)
-        {
-            _blogConfig = blogConfig.Value;
-        }
         [Authorize]
         [AllowAnonymous]
         public IActionResult Index()
         {
-            return View("Index", _blogConfig
-                .SetIsAuthenticated(User.Identity)
-                .SetLogOutUrl(Url.Action("Logout", "Account")!));
+            return View("Index");
         }
     }
 }
